209baa650e1d5d78b17961d1c3cf87daf2b10e0b
Ash Borkowicz <maybork@pm.me>
2022-06-07 03:06:36 +0200

    preserve whitespace

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from src.rpn_calc import RPNCalculator
 @click.command()
 @click.argument("expression", nargs=-1, type=str, required=True)
 def main(expression: list[str]):
-    expr = "".join(expression)
+    expr = " ".join(expression)
     print(RPNCalculator(expr).run())
 
 
