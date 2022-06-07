d29ed59ca6b28ba6f3534ead46133c0c62719882
Ash Borkowicz <maybork@pm.me>
2022-06-07 04:07:50 +0200

    add test cases stressing the error handler

--- tests/test_results.py
+++ tests/test_results.py
@@ -10,12 +10,44 @@ import src.calc
         ("1 + 4 * 2", 9),
         ("3 * (4 + 2)", 18),
         ("0.5 * (1 + 2)", 1.5),
+        ("5^2", 25),
+        ("5^2^3", 5 ** (2**3)),
+        (
+            "2^(2.5-1)",
+            2 ** (2.5 - 1),
+        ),
     ],
 )
 def test_calculate(expression: str, expected: float):
     assert RPNCalculator(expression).run() == expected
 
 
+# Test for the error handling of the calculator.
 def test_calculate_error():
     with pytest.raises(src.calc.ParserError):
         RPNCalculator("1 + 4 *").run()
+
+
+def test_calculate_error_2():
+    with pytest.raises(src.calc.ParserError):
+        RPNCalculator("1 (").run()
+
+
+def test_calculate_error_3():
+    with pytest.raises(src.calc.ParserError):
+        RPNCalculator("1 + 4 * 2 +").run()
+
+
+def test_calculate_error_4():
+    with pytest.raises(src.calc.ParserError):
+        RPNCalculator("())+").run()
+
+
+def test_calculate_error_5():
+    with pytest.raises(src.calc.ParserError):
+        RPNCalculator("())))))+").run()
+
+
+def test_point_error():
+    with pytest.raises(src.calc.TokenizerError):
+        RPNCalculator("1.2.3").run()
